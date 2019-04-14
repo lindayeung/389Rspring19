7b78a1f1a3ed1ff2b0d2e343eff9d8d9c4e10f95
lindayeung <lindayeungx@yahoo.com>
2019-04-14 00:10:12 -0400

    push

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,33 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Linda
+ * Section: 0201
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Linda Yeung
  */
 
-/* your code goes here */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int a, b;
+  a = 0xfeedface;
+  b = 0x1ceb00da;
+
+  printf("%d\n", a);
+  printf("%d\n", b);
+
+  a ^= b;
+  b ^= a;
+  a ^= b;
+
+  printf("%d\n", a);
+  printf("%d\n", b);
+}
+
+/* PART 2
+The program takes two integer variables and assigns values to them, then prints them.
+It then swaps the two variables and prints out the new swapped values!
+*/
