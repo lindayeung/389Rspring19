6759b302631675c5265d40cb01ec00a1aa2d4a94
lindayeung <lindayeungx@yahoo.com>
2019-04-27 11:58:21 -0400

    hw 9 submission

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,16 +4,20 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+
+    hashes = open("hashes.txt").read().splitlines() # open and read hashes.txt
+    passwords = open("passwords.txt").read().splitlines()# open and read passwords.txt
     characters = string.ascii_lowercase
+    for h in hashes:
+        for c in characters:
+            for p in passwords:
+                # crack hashes
+                hash = hashlib.sha256(c + p).hexdigest()
 
-    for c in characters:
-        for p in passwords:
-            # crack hashes
-
-            # print hashes as 'input:hash'
-            # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+                # print hashes as 'input:hash'
+                # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+                if h == hashlib.sha256(c + p).hexdigest():
+                    print c + p + ':' + hash
 
 if __name__ == "__main__":
     crack()
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,17 +6,29 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open("hashes.txt").read().splitlines() # open and read hashes.txt
+    passwords = open("passwords.txt").read().splitlines() # open and read passwords.txt
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
     data = s.recv(1024)
-    # parse data
+
     # crack 3 times
+    for i in range(1, 4):
+        # parse data
+        data = string.split(data, '\n')
+        data = data[2]
+
+        for c in characters:
+            for p in passwords:
+                if hashlib.sha256(c + p).hexdigest() == data:
+                    s.send(c + p + '\n')
+        data = s.recv(1024)
+
+    print(data)
 
 if __name__ == "__main__":
     server_crack()
